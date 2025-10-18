6d164740fb7ac58890ba6db5e0197447deb14a24
Andrés Padilla <andrespadillarobles@gmail.com>
2025-10-18 09:31:43 -0600

    Añadida función para aumentar una estadistica

--- generacion_terreno.h
+++ generacion_terreno.h
@@ -2,9 +2,10 @@
 #define GENERACION_TERRENO_H
 
 // Estructura que representa un territorio en el juego
-typedef struct Territorio {
+typedef struct Territorio
+{
     char codigo[3];
-    char nombre[32]; 
+    char nombre[32];
     char conexiones[4][3];
     int cantidad_conexiones;
     int A;
@@ -15,24 +16,28 @@ typedef struct Territorio {
 } Territorio;
 
 // Función para crear un nuevo territorio
-Territorio* crear_territorio(const char* codigo, const char* nombre, const char conexiones[][3], int cantidad_conexiones, int A, int B, int C);
+Territorio *crear_territorio(const char *codigo, const char *nombre, const char conexiones[][3], int cantidad_conexiones, int A, int B, int C);
 
 // Función para agregar un territorio al final de la lista
-void agregar_territorio(Territorio** cabeza, Territorio* nodo);
+void agregar_territorio(Territorio **cabeza, Territorio *nodo);
 
-//Funcion para inicializar random los valores de las problematicas
-void valoresProblematicas(Territorio* cabeza);
+// Funcion para inicializar random los valores de las problematicas
+void valoresProblematicas(Territorio *cabeza);
 
-//Funcion auxiliar para barajar (Fisher-Yates shuffle) usada en valoresProblematicas
-void barajar(int* arreglo, int n);   
+// Funcion auxiliar para barajar (Fisher-Yates shuffle) usada en valoresProblematicas
+void barajar(int *arreglo, int n);
 
 // Función para imprimir la tabla de territorios
-void imprimir_tabla(Territorio* cabeza);
+void imprimir_tabla(Territorio *cabeza);
 
 // Función para liberar la memoria de la lista enlazada
-void liberar_lista(Territorio* cabeza);
+void liberar_lista(Territorio *cabeza);
 
 // Función para construir una lista de ejemplo con 10 territorios
-Territorio* construir_lista_ejemplo(void);
+Territorio *construir_lista_ejemplo(void);
+
+// Incrementa una estadística (A, B o C) de un territorio identificado por su código.
+// Si la estadística indicada ya es 3, se incrementan en 1 las otras dos, sin pasarse de 3.
+int aumentar_estadistica(Territorio *cabeza, const char *codigo, char estadistica);
 
 #endif
